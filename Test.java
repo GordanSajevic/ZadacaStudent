d5684fc7ad9e36c02a98d5363c603823c64cba35
Gordan Sajević <gsajevic@hotmail.com>
2014-12-16 21:00:52 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		RasporedStudenata bitCamp = new RasporedStudenata();
+		bitCamp.dodavanjeStudenta();
+		bitCamp.ispis();
+	}
+
+}
